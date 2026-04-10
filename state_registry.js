540b9ca1afeb3756c30983443ef38162be9386b6
Ins0mn!a <vapors-blotchy9b@icloud.com>
2026-04-10 19:56:32 -0400

    debug

--- state_registry.js
+++ state_registry.js
@@ -8,34 +8,34 @@
 // Each entry maps a state code to its display name and data object.
 
 const STATE_REGISTRY = {
+  CA: {
+    name: 'California',
+    dataLoaded: true,
+    getData: () => CALIFORNIA_DATA   // defined in california_data.js
+  },
+  CO: {
+    name: 'Colorado',
+    dataLoaded: true,
+    getData: () => COLORADO_DATA   // defined in colorado_data.js
+  },
   FL: {
     name: 'Florida',
     dataLoaded: true,
     getData: () => FLORIDA_DATA   // defined in florida_data.js
   },
-  CO: {
-    name: 'Colorado',
-    dataLoaded: true,
-    getData: () => COLORADO_DATA
-  },
-    CA: {
-    name: 'California',
-    dataLoaded: true,
-    getData: () => CALIFORNIA_DATA
-  },
-    MA: {
+  MA: {
     name: 'Massachusetts',
     dataLoaded: true,
-    getData: () => MASSACHUSETTS_DATA
+    getData: () => MASSACHUSETTS_DATA   // defined in massachusetts_data.js
+  },
+  NJ: {
+    name: 'New Jersey',
+    dataLoaded: true,
+    getData: () => NEW_JERSEY_DATA   // defined in newjersey_data.js
   },
   NY: {
     name: 'New York',
     dataLoaded: false,
     getData: () => ({})
-  },
-  NJ: {
-    name: 'New Jersey',
-    dataLoaded: true,
-    getData: () => NEW_JERSEY_DATA
   }
 };
